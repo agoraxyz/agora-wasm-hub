d9fae73cc669872f5ce0082617662ad3e5fb745c
PopcornPaws <mark@gold.xyz>
2022-07-04 15:44:27 +0000

    Auto-generated wasm code

---
+++ tom256.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} input
+* @param {any} ring
+* @returns {any}
+*/
+export function generateProof(input: any, ring: any): any;
+/**
+* @param {any} proof
+* @param {any} ring
+* @returns {any}
+*/
+export function verifyProof(proof: any, ring: any): any;
